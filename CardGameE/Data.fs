296effd9c1cc0f8da6d6fea8619d3de76205e14e
etsinien <frumu22@gmail.com>
2016-05-16 18:03:21 +0200

    Init

---
+++ CardGameE/Data.fs
@@ -0,0 +1,8 @@
+﻿module Data
+open System
+
+[<AutoOpen>]
+type CardType = Fighter | Spell | Resource | Action
+type GamePhase = Prepare | Activate | Interfere | End
+type Players = First | Second | Third | Enemy
+type CardState = Active | Blocked | InHand | OnDesk | Used | Killed
---
+++ CardGameE/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+module Main
+open System
+open System.Windows
+
+[<STAThread>]
+[<EntryPoint>]
+let main argv = 
+    let window = WPFLib.MainWindow()
+    
+    Application().Run(window)
